f5da97d47995d2296f8aff2809d3eae01164bd08
ScottiBR <guilhermescotti@gmail.com>
2019-07-09 11:48:05 -0300

    Updates

---
+++ precache-manifest.e6903f0224623345fbe25254b49de9fe.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6774dbee90cd08ea0dee0b6c32b4779d",
+    "url": "/star-wars-quiz/index.html"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/star-wars-quiz/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "d35b076c7eb11f2e8e3e",
+    "url": "/star-wars-quiz/static/js/main.1f68f5b5.chunk.js"
+  },
+  {
+    "revision": "efb0619c00627a2cc514",
+    "url": "/star-wars-quiz/static/js/runtime~main.6eac230a.js"
+  }
+]);
